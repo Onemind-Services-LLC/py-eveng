4d4addaffad537863c725558eb378c9fb217f33f
yash-pal1 <ypal@onemindservices.com>
2025-04-21 15:27:25 +0530

    OMS-805 Test Cases Added

--- src/tests/cli/test_cli_folder.py
+++ src/tests/cli/test_cli_folder.py
@@ -41,7 +41,30 @@ class TestLabFolderCommands:
         Assert: The output indicates that folder is updated successfully.
         """
         runner: CliRunner = CliRunner()
-        result: Result = runner.invoke(cli, ["folder", "edit"])
+        # Step 1: Create folder
+        result_create = runner.invoke(
+            cli, ["folder", "create", "--name", "test_folder"]
+        )
+        assert result_create.exit_code == 0, result_create.output
+
+        # Step 2: Edit folder
+        result = runner.invoke(
+            cli,
+            [
+                "folder",
+                "edit",
+                "--folder-path",
+                "test_folder",
+                "--rename",
+                "edited_folder",
+            ],
+        )
+        assert result.exit_code == 0, result.output
+
+        # Step 3: Delete folder
+        result = runner.invoke(
+            cli, ["folder", "delete", "--folder-name", "edited_folder"]
+        )
         assert result.exit_code == 0, result.output
 
     def test_folder_delete(self):
@@ -50,5 +73,14 @@ class TestLabFolderCommands:
         Assert: The output indicates that folder is deleted successfully.
         """
         runner: CliRunner = CliRunner()
-        result: Result = runner.invoke(cli, ["folder", "delete"])
+        # Step 1: Create folder
+        result_create = runner.invoke(
+            cli, ["folder", "create", "--name", "test_folder"]
+        )
+        assert result_create.exit_code == 0, result_create.output
+
+        # Step 2: Delete folder
+        result = runner.invoke(
+            cli, ["folder", "delete", "--folder-name", "test_folder"]
+        )
         assert result.exit_code == 0, result.output
